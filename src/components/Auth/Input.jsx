b9ff3b69a0cffcd9d77d0120d65f25178b93a7e1
Faustino <Faustino@LAPTOP-VI7AL9FL>
2021-05-03 20:19:07 -0300

    form de auth, no tapa la contraseña

---
+++ src/components/Auth/Input.jsx
@@ -0,0 +1,35 @@
+import React ,{useEffect} from 'react'
+import {TextField,Grid,InputAdornment,IconButton} from '@material-ui/core'
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Visibility from "@material-ui/icons/Visibility";
+
+const Input = ({name,label,half,handleShowPassword,handleChange,type,autoFocus}) =>{
+    useEffect(() => {
+        console.log(type)
+    }, [type]);
+
+    return(
+        <Grid item xs={12} sm={half? 6 : 12}>
+            <TextField
+                name={name}
+                onChange={handleChange}
+                variant={'outlined'}
+                required
+                fullWidth
+                label={label}
+                autoFocus={autoFocus}
+                InputProps={name === 'password' ? {
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton onClick={handleShowPassword}>
+                                {type === 'password' ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                } : null}
+            />
+        </Grid>
+    )
+}
+
+export default Input
